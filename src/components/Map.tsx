277440db813daaa73b34ff5d9d780ec4b90e12f1
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-12-20 16:05:08 +0100

    add map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -9,8 +9,8 @@ const Map = ({}: PropsType) => {
         width="600"
         height="450"
         frameBorder="0"
-        style={{ border: 0 }}
-        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyABn_VpGLiSwIL0syD1P7G8NUTP2w7cZkM&q=Space+Needle,Seattle+WA"
+        style={{ border: 0, width: '100%' }}
+        src="https://www.google.com/maps/embed/v1/view?key=AIzaSyABn_VpGLiSwIL0syD1P7G8NUTP2w7cZkM&zoom=12&center=50.8195%2C3.2577"
         allowFullScreen
       ></iframe>
     </Styles.MapWrapper>
--- src/components/RegioHeader.styles.ts
+++ src/components/RegioHeader.styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components'
 
-export const RegioHeaderWrapper = styled.section``
+export const RegioHeaderWrapper = styled.section`
+ ul{
+        margin-top: 2rem; 
+
+        
+
+    }`
