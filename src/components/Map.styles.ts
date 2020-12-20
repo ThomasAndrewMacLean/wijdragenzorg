cbb7f56d2da88415d3f755ae0e4943cbc2f631cc
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-12-20 16:11:26 +0100

    full width map

--- src/components/Map.styles.ts
+++ src/components/Map.styles.ts
@@ -1,3 +1,21 @@
 import styled from 'styled-components'
 
-export const MapWrapper = styled.section``
+export const MapWrapper = styled.section`
+
+width: 100vw;
+  left: 0;
+  margin-bottom:0;
+  display: flex;
+  margin-left: calc(450px - 50vw);
+  /* filter: grayscale(0.7);
+  transition: filter 500ms ease 500ms; */
+
+ 
+  @media only screen and (max-width: 1000px) {
+    margin-left: -5vw;
+    .quoteWrap {
+      padding: 3rem;
+    }
+  }
+  
+  `
