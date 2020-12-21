39d9e017ace9dadbe0a91782eb97ec5d78a31fd7
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-12-21 16:01:33 +0100

    fix images dienstenlijst

--- src/components/DienstenLijst.styles.ts
+++ src/components/DienstenLijst.styles.ts
@@ -1,39 +1,38 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const DienstenLijstWrapper = styled.section`
-    ul{
-        margin-left:0;
-        list-style:none;
-    }
-
-`
-export const Dienst = styled.li<{even: boolean}>`
-display:flex;
-flex-direction:${props => props.even ? "row":"row-reverse"};
-@media only screen and (max-width: 660px) {
+  ul {
+    margin-left: 0;
+    list-style: none;
+  }
+`;
+export const Dienst = styled.li<{ even: boolean }>`
+  display: flex;
+  flex-direction: ${(props) => (props.even ? 'row' : 'row-reverse')};
+  @media only screen and (max-width: 660px) {
     flex-direction: column-reverse;
-}
-margin-bottom:5rem;
+  }
+  margin-bottom: 5rem;
 
-
-img{
+  img {
     width: 40%;
+    object-fit: cover;
     @media only screen and (max-width: 660px) {
-        width: 100%;
-        margin-top:2rem;
-            }
-}
-
-.text{
-    padding-left:${props => props.even ? "2rem":"0"};
-    padding-right:${props => props.even ? "0":"2rem"};
-
-    @media only screen and (max-width: 660px) {
-        padding-left:0;
-        padding-right:0;
-            }
-    h3{
-        margin-bottom:2rem;
+      width: 100%;
+      margin-top: 2rem;
     }
-}
-`
+  }
+
+  .text {
+    padding-left: ${(props) => (props.even ? '2rem' : '0')};
+    padding-right: ${(props) => (props.even ? '0' : '2rem')};
+
+    @media only screen and (max-width: 660px) {
+      padding-left: 0;
+      padding-right: 0;
+    }
+    h3 {
+      margin-bottom: 2rem;
+    }
+  }
+`;
