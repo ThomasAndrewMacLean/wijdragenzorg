53b7dcdecd9f41ee76abad8aa71640fa78ff243b
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-12-29 13:00:02 +0100

    style css

--- src/components/HomeWelcome.styles.ts
+++ src/components/HomeWelcome.styles.ts
@@ -1,42 +1,35 @@
-import styled from 'styled-components'
-
+import styled from 'styled-components';
 
 export const HomeWelcomWrapper = styled.section`
+  display: flex;
+  justify-content: space-evenly;
 
-display: flex;
-justify-content: space-evenly;
-
-button{
-    margin-top:2rem;margin-bottom:2rem;
-}
-@media only screen and (max-width: 660px) {
-        flex-direction: column;
-}
-.left{
+  button {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+  @media only screen and (max-width: 660px) {
+    flex-direction: column;
+  }
+  .left {
     width: 30%;
     @media only screen and (max-width: 660px) {
-        width: 100%;
-}
+      width: 100%;
+    }
     display: flex;
     justify-content: space-evenly;
-    flex-direction:column;
+    flex-direction: column;
     align-items: center;
 
-    p{
-        text-align:center;
-        justify-content: space-evenly;
-       
+    p {
+      text-align: center;
+      justify-content: space-evenly;
     }
-}
+  }
 
-@media only screen and (max-width: 660px) {
-      
-
-      img{
-
+  @media only screen and (max-width: 660px) {
+    img {
       max-width: 100% !important;
-      }  
-}
-
-`
- 
+    }
+  }
+`;
