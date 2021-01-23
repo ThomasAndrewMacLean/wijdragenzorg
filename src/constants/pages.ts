2fdd2b07d4a968c4d4a41873417543f74c5f179c
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-01-23 09:56:14 +0100

    cleanup

--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -1,5 +1,3 @@
-import { ComponentNameType } from '../types';
-
 export const pages: { id: string; url: string; showInNav: boolean }[] = [
   { id: 'home', url: '', showInNav: true },
   { id: 'privacy', url: 'privacy', showInNav: false },
@@ -9,4 +7,3 @@ export const pages: { id: string; url: string; showInNav: boolean }[] = [
   { id: 'contact', url: 'contact', showInNav: true },
   { id: 'covid', url: 'covid', showInNav: false },
 ];
-
