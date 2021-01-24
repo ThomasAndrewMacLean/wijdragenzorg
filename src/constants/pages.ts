d6c01986f79b25a3d9efbfd432be40815f85a82a
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-01-24 13:41:36 +0100

    hide team

--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -2,7 +2,7 @@ export const pages: { id: string; url: string; showInNav: boolean }[] = [
   { id: 'home', url: '', showInNav: true },
   { id: 'privacy', url: 'privacy', showInNav: false },
   { id: 'diensten', url: 'diensten', showInNav: true },
-  { id: 'team', url: 'team', showInNav: true },
+  { id: 'team', url: 'team', showInNav: false },
   { id: "regio's", url: 'regios', showInNav: true },
   { id: 'contact', url: 'contact', showInNav: true },
   { id: 'covid', url: 'covid', showInNav: false },
